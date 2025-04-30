40094ba4a51a99cbb8ae9941e625ebb191b3ed6b
Hamgym <pipiyakas@gmail.com>
2025-04-30 15:30:13 +0800

    obs-county api

--- backend/api/app.py
+++ backend/api/app.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 from fastapi import *
 import urllib.request as req
+import urllib.parse
 import json, ssl, os
 app = FastAPI()
 
@@ -75,3 +76,38 @@ async def get_raining(response: Response):
   return result
 
 
+@app.get("/obs-county")
+async def get_obs_county(response: Response):
+  response.headers.append("Access-Control-Allow-Origin", "*")
+  ID = "O-A0003-001"
+  KEY = os.getenv("KEY")
+  StationName = ["基隆", "臺北", "新北", "中央大學", "國一N094K", "新竹", "苗栗農改", "臺中", "田中", "日月潭", "古坑", "嘉義", "嘉義分場", "臺南", "高雄", "屏東", "宜蘭", "花蓮", "臺東", "澎湖", "金門", "馬祖"]
+  StationName = ",".join(StationName)
+  StationName = urllib.parse.quote(StationName)
+  URL = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/{ID}?Authorization={KEY}&StationName={StationName}"
+  context = ssl._create_unverified_context()
+  with req.urlopen(URL, context=context) as res:
+    resData = json.load(res)
+  records = resData["records"]
+  stations = records["Station"]
+  result = {}
+  CountyName = ['基隆市', '臺北市', '新北市', '桃園市', '新竹市', '新竹縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣', '臺南市', '高雄市', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '澎湖縣', '金門縣', '連江縣']
+  for county in CountyName:
+    result[county] = {
+      "name": -99,
+      "time": -99,
+      "temp": -99,
+      "humidity": -99,
+      "weather": -99,
+      "rain": -99,
+    }
+  for i in range(len(stations)):
+    county = stations[i]["GeoInfo"]["CountyName"]
+    name = stations[i]["StationName"]
+    result[county]["name"] = name;
+    result[county]["time"] = stations[i]["ObsTime"]["DateTime"][5:16]
+    result[county]["temp"] = stations[i]["WeatherElement"]["AirTemperature"]
+    result[county]["humidity"] = stations[i]["WeatherElement"]["RelativeHumidity"]
+    result[county]["weather"] = stations[i]["WeatherElement"]["Weather"]
+    result[county]["rain"] = stations[i]["WeatherElement"]["Now"]["Precipitation"]
+  return result
